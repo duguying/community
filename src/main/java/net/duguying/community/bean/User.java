f2e36cf3db1250ca376f229f6597eabcf64f2f21
Rex Lee <duguying2008@gmail.com>
2015-11-03 23:12:03 +0800

    try to put sql query set into Object

--- src/main/java/net/duguying/community/bean/User.java
+++ src/main/java/net/duguying/community/bean/User.java
@@ -1,5 +1,4 @@
 package net.duguying.community.bean;
-
 import net.duguying.web.orm.Pojo;
 
 /**
@@ -12,4 +11,52 @@ public class User extends Pojo {
     private String salt;
     private String email;
     private String varified;
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getSalt(){
+        return salt;
+    }
+
+    public void setSalt(String salt){
+        this.salt = salt;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getVarified(){
+        return varified;
+    }
+
+    public void setVarified(String varified){
+        this.varified = varified;
+    }
 }
--- src/main/java/net/duguying/web/orm/QueryHelper.java
+++ src/main/java/net/duguying/web/orm/QueryHelper.java
@@ -1,5 +1,6 @@
 package net.duguying.web.orm;
 
+import net.duguying.community.bean.User;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
@@ -10,9 +11,19 @@ import java.sql.SQLException;
  * Created by duguying on 2015/11/1.
  */
 public class QueryHelper {
-    public <T> T Read(Class<T> beanClass, String sql, Object... params) throws SQLException {
+    public static <T> T Read(Class<T> beanClass, String sql, Object... params) throws SQLException {
         Connection conn = DBManager.ME.getConnection();
         QueryRunner qr = new QueryRunner();
         return (T) qr.query(conn, sql, new BeanHandler(beanClass), params);
+    }
+
+    public static void main(String[] arg){
+        User user = null;
+        try {
+            user = Read(User.class, "select * from users where id=?", 8);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(user.getUsername());
     }
 }
